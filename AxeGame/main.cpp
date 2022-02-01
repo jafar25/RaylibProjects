fb38ae74c947e65ebfd5041e2f71ba4c94f9705a
Jafar <jafar.hh@hotmail.com>
2022-01-31 21:37:26 -0500

    Added circle movement

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -1,17 +1,40 @@
 #include "raylib.h"
 #include <iostream>
 
-int windowWidth{350}, windowHeight{200};
+int windowWidth{800}, windowHeight{450};
+int circle_x{200}, circle_y{200};
+int axe_x{300}, axe_y{0};
 
-bool num{true};
+int axeDeltaY{10};
 
 int main()
 {
     InitWindow(windowWidth, windowHeight, "Game Window");
-
+    SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+
+        // Game logic begins
+
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+        
+        // move the axe
+        axe_y += axeDeltaY;
+        if (axe_y > 450 || axe_y < 0)
+        {
+            axeDeltaY = -axeDeltaY;
+        }
+
+        if (IsKeyDown(KEY_D) && circle_x < windowWidth)
+        {
+            circle_x += 1;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x -= 1;
+        }
         EndDrawing();
     }
 }
